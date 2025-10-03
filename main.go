fd2bac23b6b1af80ca210460c81145385ba0e274
Claudio Dietrich <d33trik@gmail.com>
2025-10-03 08:15:46 -0300

    Implement word counting from command-line arguments
    Refactor the main function to process multiple files passed as
    command-line arguments.
    
    The program now accepts one or more filenames as command-line
    arguments and prints the word count for each file, followed by the
    total number of words if multiple files are provided.
    
    This change turns the program into a useful command-line utility for
    counting words in files, similar to the wc command.

--- main.go
+++ main.go
@@ -11,14 +11,32 @@ import (
 func main() {
 	log.SetFlags(0)
 
-	file, err := os.Open("./testdata/utf8.txt")
+	if len(os.Args) < 2 {
+		log.Fatalln("error: no file provided")
+	}
+
+	total := 0
+	filenames := os.Args[1:]
+
+	for _, filename := range filenames {
+		numberOfWords := CountWordsInFile(filename)
+		total = total + numberOfWords
+
+		fmt.Println(numberOfWords, filename)
+	}
+
+	if len(filenames) > 1 {
+		fmt.Println(total, "total")
+	}
+}
+
+func CountWordsInFile(filename string) int {
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	numberOfWords := CountWords(file)
-
-	fmt.Println(numberOfWords)
+	return CountWords(file)
 }
 
 func CountWords(r io.Reader) (numberOfWords int) {
